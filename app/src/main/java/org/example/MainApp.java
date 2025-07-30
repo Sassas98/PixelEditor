faa75e767a17d48039356ef048fdfededf13b2da
??? <marvin.sincini@studenti.unicam.it>
2025-07-30 19:55:27 +0200

    lezione renna

--- app/src/main/java/org/example/MainApp.java
+++ app/src/main/java/org/example/MainApp.java
@@ -6,6 +6,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
@@ -13,9 +18,38 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) {
         try{
+            /* 
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/aggiunta_movimenti.fxml"));
+            Parent root = loader.load();
+            Button indietro = (Button) root.lookup("#btnIndietro");
+            Button salva = (Button) root.lookup("#btnSalva");
+            TextField  causale = (TextField ) root.lookup("#txtCasuale");
+            TextField  importo = (TextField ) root.lookup("#txtImporto");
+            ChoiceBox<TipiTransazioni> tipo = (ChoiceBox<TipiTransazioni>) root.lookup("#selTipo");
+            tipo.getItems().setAll(TipiTransazioni.values());
+            tipo.setValue(TipiTransazioni.ENTRATA);
+            importo.textProperty().addListener(x -> {
+                String val = importo.getText();
+                try {
+                    Float.parseFloat(val);
+                } catch(Exception e){
+                    importo.setText("0.0");
+                }
+            });
+            indietro.setOnAction(x -> {
+                stage.close();
+            });
+            salva.setOnAction(x -> {
+                VBox box = new VBox();
+                box.getChildren().add(new Label("Causale " + causale.getText()));
+                box.getChildren().add(new Label("Importo " + importo.getText()));
+                box.getChildren().add(new Label("Tipo " + tipo.getValue().toString()));
+                Scene scene2 = new Scene(box);
+                stage.setScene(scene2);
+            });
+            */
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/index.fxml"));
             Parent root = loader.load();
-            //Button btn = (Button) root.lookup("#button");
             //Label label = (Label) root.lookup("#number");
             Scene scene = new Scene(root);
             stage.setScene(scene);
@@ -29,5 +63,10 @@ public class MainApp extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public enum TipiTransazioni{
+        ENTRATA,
+        USCITA
     }
 }
